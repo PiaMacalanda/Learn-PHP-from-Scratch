dff9cdb6ec13ce346c1bef8cb30f78a196bd0322
Pia Katleya V. Macalanda <piav.macalanda@gmail.com>
2025-02-09 17:03:16 +0800

    update

--- lesson2/lab2/challenge1.php
+++ lesson2/lab2/challenge1.php
@@ -1,3 +1,20 @@
+<?php
+ /*
+      Challenge 1: Sum of an array
+      Follow the instructions below
+
+      1. Create an array of numbers: 1,2,3,4,5
+      2. Get the sum of all of the numbers combined and put into a variable. $sum
+      3. Count the number of elements in the array and put into a variable. $quantity
+      4. Print out 'The sum of the {quantity} numbers is: {sum} '. For example, if the array is [1, 2, 3, 4, 5], the output should be 'The sum of the 5 numbers is: 15'.
+ */
+      echo '<h3> Sum of An Array</h3>';
+     //Your Solution goes here...
+
+
+
+?>
+
 
 <?php
 $header1 = 'Learn PHP from Scratch ₊˚ʚ 🌱 ₊˚✧ ﾟ.';
@@ -34,7 +51,7 @@ $output = null;
             <!-- Output -->
              <p class="text-xl"><?= $output?></p>
              <h2 class="rtext-xl font-semibold my-4">Use Array: </h2>
-             <pre><?php print_r($user); ?></pre>
+             <pre><?= print_r($user); ?></pre>
         </div>
     </div>
 </body>
